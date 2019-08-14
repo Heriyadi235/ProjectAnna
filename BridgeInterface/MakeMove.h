6f3413a697eb6bce2b1427b15144b710715fa93a
陈禹西 <30655304+Heriyadi235@users.noreply.github.com>
2019-08-14 21:00:59 +0800

    clear to upload

--- BridgeInterface/MakeMove.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void ShowGameInfo();
-int ChoseCardToPlay(int posi, int avalibleCards[13]);
-int MakeBid(int posi, int avalibleBid[]);
-int GetCard(short x, short y);
-int GetBid(short x, short y);
